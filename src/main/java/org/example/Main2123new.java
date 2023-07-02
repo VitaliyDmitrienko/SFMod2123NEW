47dbbdf1242c197ea531daac6cdc1fb88a7b33ba
VitaliyDmitrienko <vddjack2008@yandex.ru>
2023-07-02 21:56:23 +0300

    1st.

---
+++ src/main/java/org/example/Main2123new.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main2123new {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println(StringUtils.upperCase("Hello world!!!"));
+
+    }
+}
